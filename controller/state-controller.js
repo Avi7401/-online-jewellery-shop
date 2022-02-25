96b75cb94aa43f93d77c9500617d2387a8f1a572
Avi7401 <99160250+Avi7401@users.noreply.github.com>
2022-02-25 22:33:24 +0530

    Add files via upload

---
+++ controller/state-controller.js
@@ -0,0 +1,57 @@
+const StateModel = require("../model/state-model")
+
+//db insert state
+module.exports.addState = function (req, res) {
+    console.log(req.body.stateName);
+
+    let state = new StateModel({
+        stateName: req.body.stateName
+    })
+    state.save(function (err, success) {
+        if (err) {
+            res.json({ msg: "Something went wrong!!!", status: -1, data: req.body })
+        }
+        else {
+            res.json({ msg: "State added", status: 200, data: success })
+
+        }
+    })
+}
+
+
+module.exports.getAllStates = function (req, res) {
+    StateModel.find(function (err, States) {
+        if (err) {
+            res.json({ msg: "Something went wrong!!!", status: -1, data: err })
+        } else {
+            res.json({ msg: "States...", status: -1, data: States })
+        }
+    })
+}
+
+module.exports.deleteStates = function (req, res) {
+    let stateId = req.params.stateId
+
+    StateModel.deleteOne({ "_id": stateId }, function (err, data) {
+        if (err) {
+            res.json({ msg: "Something went wrong!!!", status: -1, data: err })
+        } else {
+            res.json({ msg: "state Delete...", status: -1, data: data })
+        }
+    })
+}
+
+
+module.exports.updateState = function (req, res) {
+
+    let stateId = req.params.stateId
+    let stateName = req.body.stateName
+
+    StateModel.updateOne({ _id: stateId }, { stateName: stateName }, function (err, data) {
+        if (err) {
+            res.json({ msg: "Something went wrong!!!", status: -1, data: err })
+        } else {
+            res.json({ msg: "State Update...", status: 200, data: data })
+        }
+    })
+}
